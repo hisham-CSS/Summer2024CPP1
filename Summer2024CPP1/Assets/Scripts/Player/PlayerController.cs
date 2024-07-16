f3176c8be245765f50701538a1ed69aae8d325d9
Hisham <hisham@catsplatstudios.com>
2024-07-16 11:58:14 -0400

    Lab 2 incomplete

---
+++ Summer2024CPP1/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
+    }
+}
